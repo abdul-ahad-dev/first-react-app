5687cd76ac425caf28d590fb5e58c2373b487557
abdulahad0110 <abdulahad200325@gmail.com>
2024-08-25 13:38:45 -0700

    first react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='text-center'>
+      <p className='mt-6'>Hey my name is</p>
+      <h1 className="text-5xl font-bold">ABDUL AHAD</h1>
+      <p className='mt-1'>Here is my first React Project</p>
+    </div>
+  )
+}
+
+export default App
